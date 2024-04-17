f8c60ad96013a1d29afa34dbc634231ea918b03b
Sniaper <x1xxsniaperxx1x@gmail.com>
2024-04-17 23:26:32 +0300

    First commit: adding basic project files

---
+++ src/main/java/net/sniaper/BotWorker/BotWorkerApplication.java
@@ -0,0 +1,12 @@
+package net.sniaper.BotWorker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotWorkerApplication.class, args);
+	}
+}
---
+++ src/test/java/net/sniaper/BotWorker/BotWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sniaper.BotWorker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
